31c3a104955adcfcf76d52722ea791eff5705c3b
Alhassan Yakubu <alhassan@walulel.com>
2023-03-25 00:03:59 +0000

    update mail config

--- config/mail.php
+++ config/mail.php
@@ -98,7 +98,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'yaqoubalhassan@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
