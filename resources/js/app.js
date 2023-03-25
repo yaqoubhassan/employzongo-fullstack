8ef0ebcb2473931c8ba034bc8a2e9b7aa11bf709
Alhassan Yakubu <alhassan@walulel.com>
2023-03-25 05:51:09 +0000

    use sweetalert globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ import Newsletter from '../js/components/Newsletter.vue'
 import Breadcrumb from '../js/components/Breadcrumb.vue'
 import store from './store'
 import axios from 'axios';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 
 const app = createApp(App);
 
@@ -18,6 +21,7 @@ app
 
 app.use(router)
 app.use(store)
+app.use(VueSweetalert2)
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
