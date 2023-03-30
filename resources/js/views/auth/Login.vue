066b89d07ae2cee5ee37accd23160218f044afd2
Alhassan Yakubu <alhassan@walulel.com>
2023-03-30 16:31:04 +0100

    dispatch login method

--- resources/js/views/auth/Login.vue
+++ resources/js/views/auth/Login.vue
@@ -8,18 +8,18 @@
                         <h2 class="mt-10 mb-5 text-brand-1">Member Login</h2>
                         <p class="font-sm text-muted mb-30">Access to all features. No credit card required.</p>
                         <button class="btn social-login hover-up mb-20"><img
-                                src="../../../assets/imgs/template/icons/icon-google.svg" alt="jobbox"><strong>Sign in with
+                                src="../../../assets/imgs/template/icons/icon-google.svg" alt="Google"><strong>Sign in with
                                 Google</strong></button>
                         <div class="divider-text-center"><span>Or continue with</span></div>
                     </div>
                     <form class="login-register text-start mt-20" action="#">
                         <div class="form-group">
-                            <label class="form-label" for="input-1">Username or Email address *</label>
-                            <input class="form-control" id="input-1" type="text" name="fullname" placeholder="Steven Job">
+                            <label class="form-label" for="email">Email address *</label>
+                            <input class="form-control" type="email" v-model="formData.email" placeholder="jacob@gmail.com">
                         </div>
                         <div class="form-group">
-                            <label class="form-label" for="input-4">Password *</label>
-                            <input class="form-control" id="input-4" type="password" name="password"
+                            <label class="form-label" for="password">Password *</label>
+                            <input class="form-control" type="password" v-model="formData.password"
                                 placeholder="************">
                         </div>
                         <div class="login_footer form-group d-flex justify-content-between">
@@ -30,15 +30,15 @@
                         </div>
                         <div class="form-group">
                             <button class="btn btn-brand-1 hover-up w-100" type="submit" name="login"
-                                @click.prevent="navigateToDashboard">Login</button>
+                                @click.prevent="login">Login</button>
                         </div>
                         <div class="text-muted text-center">Don't have an Account? <a href="page-signin.html">Sign up</a>
                         </div>
                     </form>
                 </div>
                 <div class="img-1 d-none d-lg-block"><img class="shape-1"
-                        src="../../../assets/imgs/page/login-register/img-4.svg" alt="JobBox"></div>
-                <div class="img-2"><img src="../../../assets/imgs/page/login-register/img-3.svg" alt="JobBox"></div>
+                        src="../../../assets/imgs/page/login-register/img-4.svg" alt="EmployZongo"></div>
+                <div class="img-2"><img src="../../../assets/imgs/page/login-register/img-3.svg" alt="EmployZongo"></div>
             </div>
         </div>
     </section>
@@ -46,10 +46,23 @@
 
 <script>
 export default {
+    data() {
+        return {
+            formData: {
+                email: '',
+                password: ''
+            }
+        }
+    },
     methods: {
-        navigateToDashboard() {
-            // window.location.reload();
-            this.$router.push('/admin')
+        login() {
+            const formData = {
+                email: this.formData.email,
+                password: this.formData.password
+            }
+            this.$Progress.start();
+
+            this.$store.dispatch('login', formData)
         }
     }
 }
