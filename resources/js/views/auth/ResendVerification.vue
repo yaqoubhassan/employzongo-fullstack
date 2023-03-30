4f3f29cc8a6d3484ad380e7c50acafeac3e5e512
Alhassan Yakubu <alhassan@walulel.com>
2023-03-30 16:31:51 +0100

    rename component

--- resources/js/views/auth/ResendVerification.vue
+++ resources/js/views/auth/ResendVerification.vue
@@ -2,18 +2,6 @@
     <div class="container text-center">
         <div class="row">
             <div class="col-xxl-6 offset-xxl-3 col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3">
-
-                <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center w-100">
-                    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                        <div class="toast-header">
-                            <strong class="me-auto">EmployZongo</strong>
-                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                        </div>
-                        <div class="toast-body">
-                            {{ alertContent }}
-                        </div>
-                    </div>
-                </div>
                 <div class="card">
                     <div class="card-header">
                         <h5>Verify Your Email Address</h5>
@@ -25,10 +13,10 @@
                         <p class="card-text">Before proceeding, please check your email for a verification link.</p>
                         <p class="card-text">If you did not receive the email</p>
                         <form class="d-inline">
-                            <div class="form-group" v-if="loading">
-                                <button class="resend-btn btn btn-primary mt-4" @click.prevent="resendVerification"
-                                    type="submit" disabled><span class="spinner-grow spinner-grow-sm" role="status"
-                                        aria-hidden="true"></span>Click here to request another</button>
+                            <div class="form-group" v-if="this.$store.state.loading">
+                                <button class="resend-btn btn btn-primary mt-4" type="submit" disabled><span
+                                        class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>Click
+                                    here to request another</button>
                             </div>
                             <div class="form-group" v-else>
                                 <button class="resend-btn btn btn-primary mt-4" @click.prevent="resendVerification"
@@ -44,30 +32,15 @@
 </template>
 
 <script>
-import axios from 'axios'
 export default {
     data() {
         return {
-            alertContent: '',
             loading: false
         }
     },
     methods: {
         async resendVerification() {
-            this.loading = true
-
-            await axios.get('auth/email/resend')
-                .then(response => {
-                    this.alertContent = response.data.data.message;
-                    this.$swal({
-                        title: 'Awesome!',
-                        text: response.data.data.message,
-                        icon: 'success',
-                        confirmButtonColor: '#3C65F5'
-                    });
-                    this.loading = false
-                })
-                .catch(error => { console.log(error) })
+            this.$store.dispatch('resendVerificationLink');
         }
     }
 }
