94f2d260698ba74d81698455de102b35800ae22f
Alhassan Yakubu <alhassan@walulel.com>
2023-03-30 16:32:23 +0100

    add login, logout and uer profile route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\VerificationController;
 
@@ -23,8 +26,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('auth')->group(function () {
     Route::post('register', RegisterController::class);
     Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
+    Route::post('login', LoginController::class)->name('auth.login');
+    Route::get('logout', LogoutController::class);
 
     Route::group(['middleware' => 'api'], function () {
         Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
     });
+});
+
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('user-profile', [UserProfileController::class, 'showProfile']);
 });
