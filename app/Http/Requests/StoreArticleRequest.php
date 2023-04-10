9979c67edc6626269db7fbb5ac3377ac0e5e20b9
Alhassan Yakubu <alhassan@walulel.com>
2023-04-10 17:16:36 +0100

    add store article request

---
+++ app/Http/Requests/StoreArticleRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'category' => 'nullable|string',
+            'image' => 'nullable|mimes:png,jpg,jpeg|max:10240'
+        ];
+    }
+}
