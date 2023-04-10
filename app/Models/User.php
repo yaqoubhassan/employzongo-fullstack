e42e1242d32348e74455b327dd14981c95003804
Alhassan Yakubu <alhassan@walulel.com>
2023-04-10 17:17:52 +0100

    add article relationship in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
