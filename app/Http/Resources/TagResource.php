11c658921adeedd5d50e50486b0b23a2686a15ae
Alhassan Yakubu <alhassan@walulel.com>
2023-04-10 17:17:02 +0100

    add tag resource

---
+++ app/Http/Resources/TagResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'category' => $this->category,
+            'created_at' => $this->created_at
+        ];
+    }
+}
